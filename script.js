4c0e1a82a45d469db6314e8993423fa14cde5d8b
Mukim Bagwan <114868361+mukim7@users.noreply.github.com>
2024-09-04 21:12:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Add class on scroll to change header background color
+window.addEventListener('scroll', function() {
+    var header = document.getElementById('header');
+    if (window.scrollY > 50) {
+        header.classList.add('header-scrolled');
+    } else {
+        header.classList.remove('header-scrolled');
+    }
+});
